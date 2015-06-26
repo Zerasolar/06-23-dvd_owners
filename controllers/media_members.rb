6b5bc419cadced04f4414c924dd49875e042730b
Kristin Houchin <kmhouchin@gmail.com>
2015-06-26 17:11:09 -0500

    On to bridge table different delete and work on stuff.

--- controllers/media_members.rb
+++ controllers/media_members.rb
@@ -9,7 +9,7 @@ end
 get "/save_media_member" do
   member = Member.new(params["name"])
   media = Media.new(params["cover_name"])
-  @new_media = MediaMember.add({"member_id" => params["name"].to_i, "media_id" => params["cover_name"].to_i})
+  @new_media_member = MediaMember.add({"member_id" => params["name"].to_i, "media_id" => params["cover_name"].to_i})
   erb :"media_members/media_member_added"
 end
 
@@ -17,10 +17,4 @@ get "/delete_media_member" do
 erb :"media_members/delete_media_member"
 end
 
-get "/gone_media_member" do
-params["media_member"].each do |media_member|
-  MediaMember.delete_row(media_member)
-end
-erb :"media_member/media_member_deleted"
-end
     
